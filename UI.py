428f31848423ddac40a91a1a7b0fd0c8ac49114a
JKR <kretschmer1982@gmx.de>
2020-07-11 14:41:26 +0200

    calculation of all relaticve prices

--- UI.py
+++ UI.py
@@ -20,6 +20,9 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
         validator.setRange(0, 50.00, 2)
         self.pizzaPrice_LI_1.setValidator(validator)
+        self.pizzaPrice_LI_2.setValidator(validator)
+        self.pizzaPrice_LI_3.setValidator(validator)
+        self.pizzaPrice_LI_4.setValidator(validator)
 
         self.length_verticalSlider.valueChanged.connect(self.set_length)
         self.width_verticalSlider.valueChanged.connect(self.set_width)
@@ -28,13 +31,33 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         price_1 = self.pizzaPrice_LI_1.text()
         price_2 = self.pizzaPrice_LI_2.text()
         price_3 = self.pizzaPrice_LI_3.text()
+        price_4 = self.pizzaPrice_LI_4.text()
         priceInCent_1 = self.price_as_float(price_1)
-        pizzaArea = float(self.pizzaSize_CB_1.currentText())**2 * math.pi / 4
+        priceInCent_2 = self.price_as_float(price_2)
+        priceInCent_3 = self.price_as_float(price_3)
+        priceInCent_4 = self.price_as_float(price_4)
+        pizzaArea_1 = float(self.pizzaSize_CB_1.currentText())**2 * math.pi / 4
+        pizzaArea_2 = float(self.pizzaSize_CB_2.currentText()) ** 2 * math.pi / 4
+        pizzaArea_3 = float(self.pizzaSize_CB_3.currentText()) ** 2 * math.pi / 4
+        pizzaArea_4 = self.length_verticalSlider.value() * self.width_verticalSlider.value()
 
-        relPrice = pizzaArea / priceInCent_1
-        self.relativePrice_Out_1.setText(str(round(relPrice, 2))) # set rel. price in textbox
+
+        relPrice_1 = pizzaArea_1 / priceInCent_1
+        relPrice_2 = pizzaArea_2 / priceInCent_2
+        relPrice_3 = pizzaArea_3 / priceInCent_3
+        relPrice_4 = pizzaArea_4 / priceInCent_4
+
+        self.relativePrice_Out_1.setText(str(round(relPrice_1, 2))) # set rel. price in textbox
+        self.relativePrice_Out_2.setText(str(round(relPrice_2, 2)))
+        self.relativePrice_Out_3.setText(str(round(relPrice_3, 2)))
+        self.relativePrice_Out_4.setText(str(round(relPrice_4, 2)))
+        self.relChange_Label_1.setText(str(round(((relPrice_2/relPrice_1)-1)*100, 2)) + " %")
+        self.relChange_Label_2.setText(str(round(((relPrice_3/relPrice_1)-1)*100, 2)) + " %")
+        self.relChange_Label_3.setText(str(round(((relPrice_4/relPrice_1)-1)*100, 2)) + " %")
 
     def price_as_float(self, text):
+        if text == "":
+            return      #ToDo add Error handling for empty fields
         text = text.replace(",", ".")
         value_float = float(text) * 100
         return value_float
