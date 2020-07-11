12d5e75db9251048b817e04427836a8e092a158b
JKR <kretschmer1982@gmx.de>
2020-07-11 23:04:38 +0200

    calculate deltas

--- UI.py
+++ UI.py
@@ -32,28 +32,56 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         price_2 = self.pizzaPrice_LI_2.text()
         price_3 = self.pizzaPrice_LI_3.text()
         price_4 = self.pizzaPrice_LI_4.text()
+        price_5 = self.pizzaPrice_LI_5.text()
         priceInCent_1 = self.price_as_float(price_1)
         priceInCent_2 = self.price_as_float(price_2)
         priceInCent_3 = self.price_as_float(price_3)
         priceInCent_4 = self.price_as_float(price_4)
+        priceInCent_5 = self.price_as_float(price_5)
         pizzaArea_1 = float(self.pizzaSize_CB_1.currentText())**2 * math.pi / 4
         pizzaArea_2 = float(self.pizzaSize_CB_2.currentText()) ** 2 * math.pi / 4
         pizzaArea_3 = float(self.pizzaSize_CB_3.currentText()) ** 2 * math.pi / 4
-        pizzaArea_4 = self.length_verticalSlider.value() * self.width_verticalSlider.value()
+        pizzaArea_4 = float(self.pizzaSize_CB_4.currentText()) ** 2 * math.pi / 4
+        pizzaArea_5 = self.length_verticalSlider.value() * self.width_verticalSlider.value()
 
 
-        relPrice_1 = pizzaArea_1 / priceInCent_1
-        relPrice_2 = pizzaArea_2 / priceInCent_2
-        relPrice_3 = pizzaArea_3 / priceInCent_3
-        relPrice_4 = pizzaArea_4 / priceInCent_4
+        relPrice_1 = priceInCent_1 / pizzaArea_1
+        relPrice_2 = priceInCent_2 / pizzaArea_2
+        relPrice_3 = priceInCent_3 / pizzaArea_3
+        relPrice_4 = priceInCent_4 / pizzaArea_4
+        relPrice_5 = priceInCent_5 / pizzaArea_5
 
         self.relativePrice_Out_1.setText(str(round(relPrice_1, 2))) # set rel. price in textbox
         self.relativePrice_Out_2.setText(str(round(relPrice_2, 2)))
         self.relativePrice_Out_3.setText(str(round(relPrice_3, 2)))
         self.relativePrice_Out_4.setText(str(round(relPrice_4, 2)))
-        self.relChange_Label_1.setText(str(round(((relPrice_2/relPrice_1)-1)*100, 2)) + " %")
-        self.relChange_Label_2.setText(str(round(((relPrice_3/relPrice_1)-1)*100, 2)) + " %")
-        self.relChange_Label_3.setText(str(round(((relPrice_4/relPrice_1)-1)*100, 2)) + " %")
+        self.relativePrice_Out_5.setText(str(round(relPrice_5, 2)))
+
+        relCngPrice_1 = round(((relPrice_2 / relPrice_1) -1) * 100, 2)
+        relCngPrice_2 = round(((relPrice_3 / relPrice_1) - 1) * 100, 2)
+        relCngPrice_3 = round(((relPrice_4 / relPrice_1) - 1) * 100, 2)
+        relCngPrice_4 = round(((relPrice_5 / relPrice_1) - 1) * 100, 2)
+
+        self.relCngPrice_Label_1.setText(str(relCngPrice_1) + " %")
+        self.relCngPrice_Label_2.setText(str(relCngPrice_2) + " %")
+        self.relCngPrice_Label_3.setText(str(relCngPrice_3) + " %")
+        self.relCngPrice_Label_4.setText(str(relCngPrice_4) + " %")
+
+        relCngArea_1 = round(((pizzaArea_2 / pizzaArea_1) -1) * 100, 2)
+        relCngArea_2 = round(((pizzaArea_3 / pizzaArea_1) -1) * 100, 2)
+        relCngArea_3 = round(((pizzaArea_4 / pizzaArea_1) -1) * 100, 2)
+        relCngArea_4 = round(((pizzaArea_5 / pizzaArea_1) -1) * 100, 2)
+
+        self.relCngArea_Label_1.setText(str(relCngArea_1) + " %")
+        self.relCngArea_Label_2.setText(str(relCngArea_2) + " %")
+        self.relCngArea_Label_3.setText(str(relCngArea_3) + " %")
+        self.relCngArea_Label_4.setText(str(relCngArea_4) + " %")
+
+        self.Area_label_1.setText(str(round(pizzaArea_1, 0)))
+        self.Area_label_2.setText(str(round(pizzaArea_2, 0)))
+        self.Area_label_3.setText(str(round(pizzaArea_3, 0)))
+        self.Area_label_4.setText(str(round(pizzaArea_4, 0)))
+        self.Area_label_5.setText(str(round(pizzaArea_5, 0)))
 
     def price_as_float(self, text):
         if text == "":
