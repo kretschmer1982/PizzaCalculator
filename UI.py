0b6a888c16fc972e8b00fae1d837a96f44d7bd70
JKR <kretschmer1982@gmx.de>
2020-07-16 13:26:43 +0200

    catch empty Inputs, hide Images

--- UI.py
+++ UI.py
@@ -11,7 +11,8 @@ class GUI(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        self.calculate_PB.clicked.connect(lambda: calculate.calculate_result(self))
+        # self.calculate_PB.clicked.connect(lambda: calculate.calculate_result(self))
+        self.calculate_PB.clicked.connect(self.check_Inputs_AndCalc)
         self.clear_PB.clicked.connect(self.clear_all_LE)
         validator = QDoubleValidator()
         validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
@@ -23,6 +24,14 @@ class GUI(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.length_verticalSlider.valueChanged.connect(self.set_length)
         self.width_verticalSlider.valueChanged.connect(self.set_width)
+
+        self.pizza_round_label.setHidden(True)
+        self.pizza_quad_label.setHidden(True)
+        self.hiddenButton_PB.clicked.connect(self.show_images)
+
+        # if self.pizzaPrice_LE_1.text() == "":
+        #     print("Textfeld 1 leer")
+
 
     def clear_all_LE(self):
         for widget in app.allWidgets():
@@ -37,6 +46,26 @@ class GUI(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def set_width(self, value):
         self.width_label.setText(str(value))
+
+    def check_Inputs_AndCalc(self):
+        for widget in app.allWidgets():
+            if isinstance(widget, QtWidgets.QLineEdit):
+                if widget.text() == "":
+                    self.missing_Input_Label.setText("Eine Eingabe fehlt")
+                    return
+        self.missing_Input_Label.setText("")
+        calculate.calculate_result(self)
+
+    def show_images(self):
+        self.pizza_round_label.setHidden(False)
+        self.pizza_quad_label.setHidden(False)
+
+        for widget in app.allWidgets():
+            if isinstance(widget, QtWidgets.QLineEdit):
+                if widget.text() == "":
+                    self.missing_Input_Label.setText("Eine Eingabe fehlt")
+
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
